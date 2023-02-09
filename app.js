26e9d14b4c6e41b5da0fccb482706a8a2257b73b
ade8m <benammaradem83@gmail.com>
2023-02-09 13:29:12 +0100

    add get &post

--- app.js
+++ app.js
@@ -2,15 +2,44 @@ const express = require ('express');
 const app = express();
 
 
+
+app.use(express.json());
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+app.post('/api/stuff',(req,res,next)=>{
+  console.log(req.body);
+  res.status(201).json({
+    message:'thing saved successfully'
+  });
+});
 
-app.use((req, res) => {
-    res.json({ message: 'Your request was successful!' }); 
+app.get('/api/stuff',(req, res,next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'My first thing',
+        description: 'All of the info about my first thing',
+        imageUrl: '',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'My second thing',
+        description: 'All of the info about my second thing',
+        imageUrl: '',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff); 
+
+   
  });
  
 
