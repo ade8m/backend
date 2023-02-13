45b527c529b0f900b1bad4eddc1e9fd56772316d
ade8m <benammaradem83@gmail.com>
2023-02-13 14:35:43 +0100

    save and retrieve data

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+// mongodb+srv://Adem:JSPROJECT@cluster0.i3ddt3r.mongodb.net/?retryWrites=true&w=majority
 const express = require ('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Thing = require('./models/thing');
 
+
+
+
+mongoose.connect('mongodb+srv://Adem:JSPROJECT@cluster0.i3ddt3r.mongodb.net/?retryWrites=true&w=majority').then(()=>{
+  console.log("connection successfully");
+})
+.catch((error)=>{
+  console.log("unable to coonect to mongoDB");
+  console.log(error);
+});
 
 
 app.use(express.json());
@@ -11,34 +25,55 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+  app.use(bodyParser.json());
 app.post('/api/stuff',(req,res,next)=>{
-  console.log(req.body);
-  res.status(201).json({
-    message:'thing saved successfully'
-  });
+    const thing = new Thing({
+      title: req.body.title,
+      description: req.body.description,
+      imageUrl: req.body.imageUrl,
+      price: req.body.price,
+      userId: req.body.userId
+    });
+    thing.save().then(()=>{
+      res.status(201).json({
+        message: 'post saved successfully!'
+      });
+    })
+      .catch((error)=> {
+        res.status(400).json({
+          error:error
+        });
+      })
+    
 });
-
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({
+    _id: req.params.id
+  }).then(
+    (thing) => {
+      res.status(200).json(thing);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+});
 app.get('/api/stuff',(req, res,next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'My first thing',
-        description: 'All of the info about my first thing',
-        imageUrl: '',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'My second thing',
-        description: 'All of the info about my second thing',
-        imageUrl: '',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff); 
-
+    Thing.find().then(
+      (things) =>{
+        res.status(200).json(things);
+      }
+    ).catch(
+      (error) =>{
+        res.status(400).json({
+          error: error
+        });
+       
+      });
+    
    
  });
  
